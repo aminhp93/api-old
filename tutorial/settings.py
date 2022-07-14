df4fd21ffcbcd4c9351b3cc73c327ff1ec8cc1e3
Minh  Pham <minhpham@Minhs-MacBook-Pro.local>
2022-07-14 16:51:45 +0700

    Update

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9vbw40p$-v719@d*cf51w84&1ta@tw7-%5_wo(ky22m)r)*^%d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
