b45bbedba931ae2f8bd618c8b466f59c4819753e
Minh  Pham <minhpham@Minhs-MacBook-Pro.local>
2022-07-19 12:08:14 +0700

    Update

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'api',
+        'USER': 'aminhp93',
+        'PASSWORD': 'Aminhp93',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
