41c06d95700c962450d7d7e85dbe94336d3e1b8b
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2022-07-29 15:28:00 +0700

    Update

--- chats/urls.py
+++ chats/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import (
     ChatList,
-    ChatDetail
+    ChatDetail,
+    ChatViewSet,
+    PuhserAuthenticationAPI
 )
 
 app_name = "chats"
@@ -9,4 +11,8 @@ app_name = "chats"
 urlpatterns = [
     path('', ChatList.as_view()),
     path('<int:pk>/', ChatDetail.as_view()),
+    path('pusher/auth/', PuhserAuthenticationAPI.as_view({ 'post': 'pusher_authentication'})),
+    # path('', ChatViewSet.as_view({'get': 'list'})),
+    # path('<int:pk>/', ChatViewSet.as_view({'get': 'retrieve'})),
+
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,58 +1,59 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 import pusher
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import (
-    ListAPIView,
-    
+    ListAPIView,   
 )
 from rest_framework.permissions import (
     IsAuthenticated,
 )
-from rest_framework import status
+from rest_framework import status, viewsets
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 
 from .models import Chat
 from .serializers import ChatSerializer
 from .paginations import ChatLimitOffsetPagination
 
 # Create your views here.
-
-class ChatList(APIView):
+PUSHER_CONFIG = {
+    'app_id': '1435319',
+    'key': '15ee77871e1ed5258044',
+    'secret': '4c2b97bec11d18dc8a13',
+    'cluster': 'ap1',
+}
+class ChatList(ListAPIView):
     """
     List all chats, or create a new chat.
     """
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
     permission_classes = [IsAuthenticated]
-    pagination_class = ChatLimitOffsetPagination
+    # pagination_class = ChatLimitOffsetPagination
 
     def get(self, request, *args, **kwargs):
         chats = Chat.objects.all()
         serializer = ChatSerializer(chats, many=True)
         return Response(serializer.data)
 
+
     def post(self, request, format=None):
         serializer = ChatSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(sender=request.user)
-            
-            PUSHER_CONFIG = {
-                'app_id': '1435319',
-                'key': '15ee77871e1ed5258044',
-                'secret': '4c2b97bec11d18dc8a13',
-                'cluster': 'ap1',
-            }
             
             pusher_client = pusher.Pusher(
                 app_id=PUSHER_CONFIG['app_id'],
                 key=PUSHER_CONFIG['key'],
                 secret=PUSHER_CONFIG['secret'],
                 cluster=PUSHER_CONFIG['cluster']
-                )
+            )
 
             pusher_client.trigger('chat', 'message', serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
 
 class ChatDetail(APIView):
@@ -73,6 +74,7 @@ class ChatDetail(APIView):
         chat = self.get_object(pk)
         serializer = ChatSerializer(chat)
         return Response(serializer.data)
+        
 
     def put(self, request, pk, format=None):
         chat = self.get_object(pk)
@@ -88,3 +90,47 @@ class ChatDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     
+class ChatViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
+    # queryset = Chat.objects.all()
+    # serializer_class = ChatSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = ChatLimitOffsetPagination
+
+    def list(self, request):
+        queryset = Chat.objects.all()
+        serializer = ChatSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Chat.objects.all()
+        chat = get_object_or_404(queryset, pk=pk)
+        serializer = ChatSerializer(chat)
+        return Response(serializer.data)
+
+
+class PuhserAuthenticationAPI(ModelViewSet):
+
+    @action(detail=True, method=['post'])
+    def pusher_authentication(self, request):
+        print(request)
+        pusher_client = pusher.Pusher(
+            app_id=PUSHER_CONFIG['app_id'],
+            key=PUSHER_CONFIG['key'],
+            secret=PUSHER_CONFIG['secret'],
+            cluster=PUSHER_CONFIG['cluster']
+        )
+        auth = pusher_client.authenticate(
+            channel=request.data.get('channel_name'),
+            socket_id=request.data.get('socket_id'),
+            custom_data={
+                'user_id': request.user.id,
+                'user_info': {
+                    'username': request.user.username,
+                }
+            }
+        )
+
+        return Response(auth, status=status.HTTP_200_OK)
