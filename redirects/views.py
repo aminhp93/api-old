db0b7019c6a96d52add113e6d80a13760c8413e9
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2022-12-15 15:45:15 +0700

    Update redirect url

--- redirects/views.py
+++ redirects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests
-
+import json
 # Create your views here.
 
 class Redirect2View(APIView):
@@ -21,17 +21,16 @@ class Redirect2View(APIView):
 class RedirectView(APIView):
     def get(self, request, *args, **kwargs):
         try:
-            url = request.GET.get('redirect_url')
+            url = request.GET.get('url')
+            method = request.GET.get('method')
+            headers = request.GET.get('headers')
+            payload = request.GET.get('payload')
+            headers = json.loads(headers)
 
             if not url:
-                return Response({'errors': 'No url'})
+                return Response({'errors': 'No url'}, status=400)
 
-
-            payload={}
-            headers = {}
-
-            response = requests.request("GET", url, headers=headers, data=payload)
+            response = requests.request(method, url, headers=headers, data=payload)
             return Response(response.json())
         except:
-            return Response({'errors': 'Error'})
-       
+            return Response({'errors': 'Error'}, status=400)
